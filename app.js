a26ef922230687b71b60a633e321d6a1cf90ec05
veisvidyte <veisvidyte@gmail.com>
2023-07-31 16:26:30 +0100

    removed js

--- app.js
+++
@@ -1,8 +0,0 @@
-function sendAlert {
-    prompt("Please enter your name")
-    prompt("Please enter your email address")
-    alert(`Thank you ${name}, you will be notified!`)
-}
-
-let notifyMeButton = document.querySelector("#notify-me-button")
-notifyMeButton.addEventListener("click", sendAlert)
